643d2b3149c902219a89bbcd6188eb7bdb459432
wangxingang <wangxingang0610@sina.com>
2019-02-08 17:16:20 +0800

    Initial commit

---
+++ src/main/java/com/study/SpringBoot03LoggingSlf4jLog4j2Application.java
@@ -0,0 +1,30 @@
+package com.study;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot03LoggingSlf4jLog4j2Application {
+
+    //记录器
+    static Logger logger = LoggerFactory.getLogger(SpringBoot03LoggingSlf4jLog4j2Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot03LoggingSlf4jLog4j2Application.class, args);
+
+        //日志的级别；
+        //由低到高   trace<debug<info<warn<error
+        //可以调整输出的日志级别；日志就只会在这个级别以以后的高级别生效
+        logger.trace("这是trace日志...");
+        logger.debug("这是debug日志...");
+
+        //SpringBoot默认给我们使用的是info级别的，没有指定级别的就用SpringBoot默认规定的级别；root级别
+        logger.info("这是info日志...");
+        logger.warn("这是warn日志...");
+        logger.error("这是error日志...");
+    }
+
+}
+
---
+++ src/test/java/com/study/SpringBoot03LoggingSlf4jLog4j2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.study;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot03LoggingSlf4jLog4j2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
